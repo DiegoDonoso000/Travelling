c4c159cb6692bd94eb68e4c45f8a9d7d54e0990f
Diego <ddonoso000@gmail.com>
2023-01-17 19:47:14 +0100

    Actualizacion global

--- app/src/main/java/es/travelWorld/travelling/MainActivity.java
+++ app/src/main/java/es/travelWorld/travelling/MainActivity.java
@@ -3,12 +3,18 @@ package es.travelWorld.travelling;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.Button;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+//        setContentView(R.layout.home);
+        setContentView(R.layout.onboarding);
     }
+
 }
