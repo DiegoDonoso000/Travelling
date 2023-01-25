8a47fb9465b234755f9331ef836fe485d1aeed28
Diego <ddonoso000@gmail.com>
2023-01-25 20:50:40 +0100

    Mejoras en gestion de Status Bar y Cabecera de aplicación

--- app/src/main/java/es/travelWorld/travelling/MainActivity.java
+++ app/src/main/java/es/travelWorld/travelling/MainActivity.java
@@ -1,20 +1,42 @@
 package es.travelWorld.travelling;
 
+import androidx.annotation.LayoutRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.core.view.WindowCompat;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.transition.Transition;
+import android.transition.TransitionInflater;
+import android.transition.TransitionManager;
+import android.util.Log;
 import android.view.Window;
 import android.widget.Button;
 
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+    private boolean isDetailExpand;
+    ConstraintLayout constraintLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-//        setContentView(R.layout.home);
+        Window window = this.getWindow();
+        window.setStatusBarColor(this.getColor(R.color.status_bar_onboarding));
         setContentView(R.layout.onboarding);
+        //Referenciar boton
+        MaterialButton btn = findViewById(R.id.onboard_button_next);
+        //Referenciar ConstraintLayout
+        constraintLayout= findViewById(R.id.onboarding_activity);
+        // escuchar el boton
+        btn.setOnClickListener(view -> actionPulseBtn());
+    }
+    private void actionPulseBtn() {
+        Window window = this.getWindow();
+        window.setStatusBarColor(this.getColor(R.color.status_bar_home));
+        setContentView(R.layout.home);
     }
 
 }
