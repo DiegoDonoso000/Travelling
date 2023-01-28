6f9ca0147c26831b5de239e07b26da1f47f5e976
Diego <ddonoso000@gmail.com>
2023-01-28 18:46:55 +0100

    Trabajar con viewBindings

--- app/src/main/java/es/travelWorld/travelling/MainActivity.java
+++ app/src/main/java/es/travelWorld/travelling/MainActivity.java
@@ -1,46 +1,66 @@
 package es.travelWorld.travelling;
 
-import androidx.annotation.LayoutRes;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.constraintlayout.widget.ConstraintSet;
-import androidx.core.view.WindowCompat;
-
-import android.app.Activity;
 import android.os.Bundle;
-import android.transition.Transition;
-import android.transition.TransitionInflater;
-import android.transition.TransitionManager;
-import android.util.Log;
+import android.view.View;
 import android.view.Window;
-import android.widget.Button;
-
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import es.travelWorld.travelling.databinding.ActivityMainBinding;
+import es.travelWorld.travelling.databinding.HomeBinding;
+import es.travelWorld.travelling.databinding.LoginBinding;
+import es.travelWorld.travelling.databinding.OnboardingBinding;
 
 public class MainActivity extends AppCompatActivity {
     private boolean isDetailExpand;
-    ConstraintLayout constraintLayout;
+    View view;
+    ActivityMainBinding bindingMainActivity;
+    OnboardingBinding bindingOnboard;
+    LoginBinding bindingLogin;
+    HomeBinding bindingHome;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
+        bindingMainActivity= ActivityMainBinding.inflate(getLayoutInflater());
+        bindingOnboard=OnboardingBinding.inflate(getLayoutInflater());
+        bindingHome= HomeBinding.inflate(getLayoutInflater());
+        bindingLogin=LoginBinding.inflate(getLayoutInflater());
 
         Window window = this.getWindow();
         window.setStatusBarColor(this.getColor(R.color.status_bar_onboarding));
-        setContentView(R.layout.onboarding);
-        //Referenciar boton
-        MaterialButton btn = findViewById(R.id.onboard_button_next);
-        //Referenciar ConstraintLayout
-        constraintLayout= findViewById(R.id.onboarding_activity);
-        // escuchar el boton
-        btn.setOnClickListener(view -> actionPulseBtn());
+        setContentView(bindingOnboard.getRoot());
+        setListeners();
     }
+
+    private void setListeners() {
+        // escuchar el boton
+        bindingOnboard.onboardButtonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                actionPulseBtn();
+            }
+        });
+        bindingLogin.pwdForgotActionText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view,R.string.in_construction_text,Snackbar.LENGTH_LONG).show();
+            }
+        });
+        bindingLogin.getAccountActionText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view,R.string.in_construction_text,Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+
     private void actionPulseBtn() {
         Window window = this.getWindow();
         window.setStatusBarColor(this.getColor(R.color.status_bar_home));
-        setContentView(R.layout.login);
+        setContentView(bindingLogin.getRoot());
+//        setContentView(bindingHome.getRoot());
     }
 
 }
